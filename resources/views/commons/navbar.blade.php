82b843ea31b889317f17faea842390685205fe41
mitsuki.yamamoto <2191205@shodai.ac.jp>
2019-06-05 08:40:29 +0000

    user registration

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -13,6 +13,9 @@
                 <li class="nav-item"><a href="#" class="nav-link">Signup</a></li>
                 <li class="nav-item"><a href="#" class="nav-link">Login</a></li>
             </ul>
-        </div>
+            <ul class="nav navbar-nav navbar-right">
+                    <li>{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
+                    <li><a href="#">Login</a></li>
+            </ul> 
     </nav>
 </header>
